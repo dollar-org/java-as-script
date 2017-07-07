5c007c4df046e2d4283e4c9f8564d255ae3c1e04
neilellis <neil@vizz.buzz>
2017-07-07 15:34:09 +0100

    Moving over to Maven

---
+++ src/main/java/com/innowhere/relproxy/impl/jproxy/core/clsmgr/comp/jfo/JProxyJavaFileObjectInput.java
@@ -0,0 +1,10 @@
+package com.innowhere.relproxy.impl.jproxy.core.clsmgr.comp.jfo;
+
+/**
+ *
+ * @author jmarranz
+ */
+public interface JProxyJavaFileObjectInput
+{
+    public String getBinaryName();
+}
