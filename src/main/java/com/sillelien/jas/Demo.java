140137b83cfa49e24b2cbe3c335c0a6f692a578f
neilellis <neil@vizz.buzz>
2017-07-11 18:43:16 +0100

    Bintray work

--- src/main/java/com/sillelien/jas/Demo.java
+++ src/main/java/com/sillelien/jas/Demo.java
@@ -5,6 +5,7 @@ import com.sillelien.jas.jproxy.JProxyConfig;
 import com.sillelien.jas.jproxy.JProxyScriptEngine;
 import com.sillelien.jas.jproxy.JProxyScriptEngineFactory;
 
+import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.tools.Diagnostic;
@@ -37,7 +38,8 @@ public class Demo {
 
         ScriptEngineManager manager = new ScriptEngineManager();
         manager.registerEngineName("java", factory);
-        manager.getBindings().put("in", "World");
+        Bindings bindings = manager.getBindings();
+        bindings.put("in", "World");
 
         ScriptEngine engine = manager.getEngineByName("java");
         JProxyScriptEngine scriptEngine = (JProxyScriptEngine) engine;
@@ -45,7 +47,7 @@ public class Demo {
 
         //Your code goes here, e.g.
 
-        scriptEngine.eval("System.out.println(\"Hello \"+context.getAttribute(\"in\",javax.script.ScriptContext.ENGINE_SCOPE));return null;\n");
+        scriptEngine.eval("System.out.println(\"Hello \"+context.getAttribute(\"in\",javax.script.ScriptContext.ENGINE_SCOPE));return null;\n",bindings);
 
     }
 }
